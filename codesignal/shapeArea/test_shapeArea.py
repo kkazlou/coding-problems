5667c3c456917002ea88588931afe64c03eebfb9
kkazlou <59448734+kkazlou@users.noreply.github.com>
2023-06-16 18:48:08 -0500

    added shapeArea

---
+++ codesignal/shapeArea/test_shapeArea.py
@@ -0,0 +1,11 @@
+import pytest
+from shapeArea import *
+
+def test_shapeArea():
+    assert solution(2) == 5
+    assert solution(3) == 13
+    assert solution(1) == 1
+    assert solution(5) == 41
+    assert solution(7000) == 97986001
+    assert solution(8000) == 127984001
+    assert solution(9999) == 199940005
