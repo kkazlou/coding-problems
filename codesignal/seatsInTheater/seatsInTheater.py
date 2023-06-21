8709524a1b18ac3eb588e457f47f365880a23e63
kkazlou <59448734+kkazlou@users.noreply.github.com>
2023-06-20 22:13:37 -0500

    added seatsInTheater

---
+++ codesignal/seatsInTheater/seatsInTheater.py
@@ -0,0 +1,5 @@
+def solution(nCols, nRows, col, row):
+    x = nCols - col + 1
+    y = nRows - row
+    
+    return x*y
---
+++ codesignal/seatsInTheater/test_seatsInTheater.py
@@ -0,0 +1,9 @@
+import pytest
+from seatsInTheater import *
+
+def test_seatsInTheater():
+    assert solution(16, 11, 5, 3) == 96
+    assert solution(1, 1, 1, 1) == 0
+    assert solution(13, 6, 8, 3) == 18
+    assert solution(60, 100, 60, 1) == 99
+    assert solution(1000, 1000, 1000, 1000) == 0
